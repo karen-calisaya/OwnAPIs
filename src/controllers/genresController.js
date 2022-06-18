8d3c1f6b4b429ba1c3e4f0f3f1e738f70745d89c
Karen Calisaya <karenmcalisaya@gmail.com>
2022-06-18 15:27:14 -0300

    fetch

--- src/controllers/genresController.js
+++ src/controllers/genresController.js
@@ -1,13 +1,17 @@
 const db = require('../database/models');
 const sequelize = db.sequelize;
+const fetch = require('node-fetch')
+
 
 
 const genresController = {
     'list': (req, res) => {
-        db.Genre.findAll()
-            .then(genres => {
-                res.render('genresList.ejs', {genres})
-            })
+        fetch('http://localhost:3001/api/genres') /* pedicion get, si no lo aclaramos es get por defecto */
+        .then(res => res.json())
+        .then(result => {
+            res.render('genresList.ejs', {genres: result.data})
+        })
+        /* esto lo hacemos para consumir apis de terceros, no aplicaciones nuestras */
     },
     'detail': (req, res) => {
         db.Genre.findByPk(req.params.id)
--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -2,7 +2,6 @@ const path = require('path');
 const db = require('../database/models');
 const sequelize = db.sequelize;
 const { Op } = require("sequelize");
-const moment = require('moment');
 
 
 //Aqui tienen otra forma de llamar a cada uno de los modelos
